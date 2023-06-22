847362dec339c5ca0186c18bb0be43300de6d914
Roi Yomtovyan <37290526+RoiYomtovyan@users.noreply.github.com>
2023-06-22 15:29:39 +0300

    remove code

--- src/SanityTest.java
+++ src/SanityTest.java
@@ -1,12 +1,6 @@
 import org.junit.*;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-
-import java.util.concurrent.TimeUnit;
-import static org.junit.Assert.fail;
-
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.WebDriver;
@@ -48,57 +42,6 @@ public class SanityTest {
         driver.get("https://magento.softwaretestingboard.com/");
 
     }
-
-       //  the before test is using "ReadBrowserAndURLFromFile" class to read the "browserType" parameter to define which type of browser driver to use
-        // the condition is enveloped with "try catch" that write the result in the report
-
-//        boolean driverEstablish = false;
-//        try {
-//            String browserType = ReadBrowserAndURLFromFile.getData("browserType");
-//            if (browserType.equals("Chrome")) {
-//                System.setProperty("webdriver.chrome.driver", "C:\\Users\\avivit\\Documents\\GitHub\\BuyMeProject\\chromedriver.exe");
-//                ChromeOptions options = new ChromeOptions();
-//                driver = new ChromeDriver(options);
-//            } else if (browserType.equals("IE")) {
-//                driver = new InternetExplorerDriver();
-//            } else if (driver == null) {
-//                WebElement e = driver.findElement(By.id("123"));
-//                test.log(Status.FATAL, e.getText());
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            fail("Cant connect to Driver");
-//            test.log(Status.FATAL, "Driver Connection Failed! " + e.getMessage());
-//            driverEstablish = false;
-//        } finally {
-//            if (driverEstablish == true) {
-//                test.log(Status.PASS, "Driver established successfully");
-//
-//            }
-//        }
-//        //  the before test is using "ReadBrowserAndURLFromFile" class to read the "URL" parameter that define the site URL
-//        // the condition is enveloped with "try catch" that write the result in the report
-//        boolean pageOpened = false;
-//        try {
-//            driver.navigate().to(ReadBrowserAndURLFromFile.getData("url"));
-//            pageOpened = true;
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            test.log(Status.FAIL, "Buyme webpage was not found " + e.getMessage());
-//            pageOpened = false;
-//        } finally {
-//            if (pageOpened) {
-//                test.log(Status.PASS, "Buyme" + "Webpage opened successfully");
-//
-//            }
-//
-//            // this will wait up to 10 sec to verify that the elemand is displayed
-//            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//
-//        }
-
-
 
 @Test
 // this test is verifying the registration of new user and report the result in the report
